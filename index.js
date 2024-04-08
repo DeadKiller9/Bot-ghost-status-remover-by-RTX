fb1ab1debba82bf28429a43f614027df6cdb118e
DeadKiller9 <166442037+DeadKiller9@users.noreply.github.com>
2024-04-08 23:18:35 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["EnderNW Listening","EnderNW Watching"];
 
 
 let currentIndex = 0;
